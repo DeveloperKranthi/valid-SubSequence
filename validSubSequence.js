de0825558cc9fb56657415503d17c99fc86cb3c1
G Krnathi Kumar <49958256+DeveloperKranthi@users.noreply.github.com>
2021-01-31 00:07:37 +0530

    Update validSubSequence.js

--- validSubSequence.js
+++ validSubSequence.js
@@ -6,6 +6,7 @@ function isValidSubsequence(array, sequence) {
     if (array[arrIdx] === sequence[seqIdx]) seqIdx++;
     arrIdx++;
   }
+  
   return seqIdx === sequence.length;
 }
 isValidSubsequence([5, 1, 22, 25, 6, -1, 8, 10], [1, 6, -1, 10]);
